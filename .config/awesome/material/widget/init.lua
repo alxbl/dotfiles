9d18b2e9803185292793562a2caeb0e3220ccc4e
Alexandre Beaulieu <alex@segfault.me>
2018-09-25 19:31:43 -0400

    chore(awesome): Cleaned up old awesome config. Starting from scratch.

--- .config/awesome/material/widget/init.lua
+++
@@ -1,6 +0,0 @@
-local wrequire     = require("lain.helpers").wrequire
-local setmetatable = setmetatable
-
-local widget = { _NAME = "material.widget" }
-
-return setmetatable(widget, { __index = wrequire })
