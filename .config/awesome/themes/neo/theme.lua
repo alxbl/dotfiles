60cfaa554d6285fc7f8ab3f4fe6fcbd8d7478e23
Alexandre Beaulieu <alex@segfault.me>
2018-10-22 17:52:01 -0400

    fix(wm): Apparently forgot to commit theme file all this time!

---
+++ .config/awesome/themes/neo/theme.lua
@@ -0,0 +1,70 @@
+-- vim:foldmethod=marker
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+local HOME = os.getenv("HOME")
+
+local theme = {}
+
+theme.font          = "Noto sans 8"
+
+-- Colors {{{
+local accent = "#33aadd"
+local dark =   "#212121"
+theme.bg_normal     = dark
+-- theme.bg_focus      = "#535d6c"
+theme.bg_focus      = accent
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#444444"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#a1a1a1"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = theme.fg_normal
+
+theme.useless_gap   = dpi(10)
+theme.border_width  = dpi(1)
+theme.border_normal = "#000000"
+theme.border_focus  = accent
+theme.border_marked = "#91231c"
+theme.wallpaper = HOME .. "/.config/awesome/wall.png"
+theme.tasklist_bg_focus = dark -- No accent on active task
+-- }}}
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
+
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+
+-- Layout Widget {{{
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+-- }}}
+
+return theme
+
