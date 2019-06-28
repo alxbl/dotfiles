2b240e89017a653aec52bb3f72e5c7f23f1c1f54
Alexandre Beaulieu <alex@segfault.me>
2019-06-28 13:07:34 -0400

    feat: org-mode bindings.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/.emacs.d/org/main.org")))
  '(package-selected-packages
    (quote
     (python-mode rust-mode magit linum-relative-mode evil-mode ele linum-relative use-package helm evil-visual-mark-mode dracula-theme))))
