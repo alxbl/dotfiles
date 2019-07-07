65408dba59528528779344b2dd16e9a241e655b6
Alexandre Beaulieu <alex@segfault.me>
2019-07-07 12:07:39 -0400

    chore: gitignore

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Code/alxbl/dotfiles/.emacs.d/settings.org" "~/.emacs.d/org/main.org")))
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
     (helm-lsp company-lsp lsp-ui treemacs diminish yasnippet-snippets helm-projectile projectile flycheck company yasnippet lsp-mode evil-surround evil-commentary python-mode rust-mode magit linum-relative-mode evil-mode ele linum-relative use-package helm evil-visual-mark-mode dracula-theme))))
