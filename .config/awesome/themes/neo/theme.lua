03a697ab3f5f74c31e701ec7fc56bcd3f4ca6e26
Alexandre Beaulieu <alex@segfault.me>
2019-09-30 13:24:49 -0400

    cleanup: neo - spacing.

--- .config/awesome/themes/neo/theme.lua
+++ .config/awesome/themes/neo/theme.lua
@@ -1,39 +1,37 @@
--- vim:foldmethod=marker
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
+local settings = require("settings")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-local HOME = os.getenv("HOME")
-
 local theme = {}
 
-theme.font          = "Noto sans"
+theme.font = "Noto sans"
 
--- Colors {{{
-local accent = "#33aadd"
-local dark =   "#212121"
+local accent = "#6272a4"
+local dark =   "#282a36"
+local foreground = "#f8f8f2"
+
 theme.bg_normal     = dark
 -- theme.bg_focus      = "#535d6c"
 theme.bg_focus      = accent
-theme.bg_urgent     = "#ff0000"
+theme.bg_urgent     = "#ff5555"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#a1a1a1"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
+theme.fg_normal     = foreground
+theme.fg_focus      = foreground
+theme.fg_urgent     = foreground
 theme.fg_minimize   = theme.fg_normal
 
-theme.useless_gap   = dpi(8)
-theme.border_width  = 0
-theme.border_normal = "#000000"
-theme.border_focus  = accent
-theme.border_marked = "#91231c"
+theme.useless_gap   = dpi(4)
+theme.border_width  = 1
+theme.border_normal = "#282a36"
+theme.border_focus  = "#bd93f9"
+theme.border_marked = "#f1fa8c"
 theme.wallpaper = HOME .. "/.config/awesome/wall.png"
 theme.tasklist_bg_focus = dark -- No accent on active task
--- }}}
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
@@ -46,32 +44,29 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_
 -- notification_[width|height|margin]
 -- notification_[border_color|border_width|shape|opacity]
 
--- Layout Widget {{{
-theme.lain_icons         = os.getenv("HOME") ..  "/.config/awesome/lain/icons/layout/default/"
+theme.lain_icons         = settings.HOME ..  "/.config/awesome/lain/icons/layout/default/"
 theme.layout_termfair    = theme.lain_icons .. "termfair.png"
 theme.layout_centerfair  = theme.lain_icons .. "centerfair.png"  -- termfair.center
 theme.layout_cascade     = theme.lain_icons .. "cascade.png"
 theme.layout_cascadetile = theme.lain_icons .. "cascadetile.png" -- cascade.tile
 theme.layout_centerwork  = theme.lain_icons .. "centerwork.png"
 theme.layout_centerworkh = theme.lain_icons .. "centerworkh.png" -- centerwork.horizontal
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_fairh       = themes_path      .. "default/layouts/fairhw.png"
+theme.layout_fairv       = themes_path      .. "default/layouts/fairvw.png"
 
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
--- }}}
+theme.layout_floating   = themes_path .. "default/layouts/floatingw.png"
+theme.layout_magnifier  = themes_path .. "default/layouts/magnifierw.png"
+theme.layout_max        = themes_path .. "default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path .. "default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path .. "default/layouts/tileleftw.png"
+theme.layout_tile       = themes_path .. "default/layouts/tilew.png"
+theme.layout_tiletop    = themes_path .. "default/layouts/tiletopw.png"
+theme.layout_spiral     = themes_path .. "default/layouts/spiralw.png"
+theme.layout_dwindle    = themes_path .. "default/layouts/dwindlew.png"
+theme.layout_cornernw   = themes_path .. "default/layouts/cornernww.png"
+theme.layout_cornerne   = themes_path .. "default/layouts/cornernew.png"
+theme.layout_cornersw   = themes_path .. "default/layouts/cornersww.png"
+theme.layout_cornerse   = themes_path .. "default/layouts/cornersew.png"
 
 return theme
-
