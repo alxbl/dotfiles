e4757b82dc9b478def8b16b2d3757d1a0bb8fe06
Alexandre Beaulieu <alex@segfault.me>
2022-02-28 21:07:47 -0500

    feat: auto-set screenlayout if found

--- .config/autorun.sh
+++ .config/autorun.sh
@@ -4,3 +4,9 @@ fcitx &
 flameshot &
 xautolock -time 5 -locker  "i3lock -c1f67b1 -u -i ~/.config/awesome/lock.png" &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+polybar &
+
+LAYOUT="$HOME/.screenlayout/default.sh"
+if [ -x $LAYOUT]; then
+    $LAYOUT
+fi
