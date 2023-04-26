d78d5d0f5c41556a87b341f9bab3534ddea9b2ee
Alexandre Beaulieu <alex@segfault.me>
2023-04-26 09:17:19 -0400

    cleanup: move i3 scripts to .config/i3

---
+++ .config/i3/4k-dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --mode 1920x1080 --pos 3840x1080 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
