599b45787931cf44aa2e1ee49802e5ffccd826d6
Alexandre Beaulieu <alex@segfault.me>
2024-04-06 20:58:55 -0400

    revert to settings.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,2 @@
-(org-babel-load-file (expand-file-name  "~/.emacs.d/clean.org"))
+(org-babel-load-file (expand-file-name  "~/.emacs.d/settings.org"))
 ;(org-babel-load-file (expand-file-name  "~/.emacs.d/tmp.org"))
